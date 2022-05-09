31a73657588e3f7b7cc372cc61aa5b11dd41f257
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-05-09 12:48:02 -0230

    Add components and css files

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from '../Nav';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <Nav></Nav>
+    </div>
+  )
+}
